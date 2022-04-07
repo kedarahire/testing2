243415f11cdf64a1dd3c7cf03ae82975bca2a65a
kedarahire <kedarsing88@gmail.com>
2022-04-07 14:46:33 +0530

    FIRST COMMIT

---
+++ src/githubstudy/GithubClass.java
@@ -0,0 +1,20 @@
+package githubstudy;
+
+public class GithubClass {
+
+	public static void main(String[] args) {
+		
+		GithubClass.addition();
+		System.out.println("hello");
+
+	}
+	
+	public static  void addition()
+	{
+		int a =10;
+		int b=10;
+		int sum = a+b;
+		System.out.println(sum);
+	}
+
+}
